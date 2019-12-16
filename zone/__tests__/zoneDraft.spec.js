753ebadccc4f59339ff5523f33d10b2afc816706
Federico Moro del Álamo <fede@devgurus.io>
2019-12-16 19:32:35 +0100

    Add zone tests

---
+++ zone/__tests__/zoneDraft.spec.js
@@ -0,0 +1,66 @@
+const Ajv = require("ajv");
+const schema = require("../zoneDraft");
+
+describe("zoneDraft", () => {
+  let ajv;
+  let data;
+  let valid;
+
+  describe("when success", () => {
+    describe("when only required fields", () => {
+      beforeAll(() => {
+        data = { name: "name" };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        expect(valid).toBeTruthy();
+      });
+    });
+
+    describe("when all fields", () => {
+      beforeAll(() => {
+        data = {
+          key: "key",
+          name: "name",
+          description: "description",
+          locations: [{ country: "US", state: "CA" }]
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        expect(valid).toBeTruthy();
+      });
+    });
+  });
+
+  describe("when error", () => {
+    describe("when missing name property", () => {
+      beforeAll(() => {
+        data = {
+          key: "key",
+          description: "description",
+          locations: [{ country: "US", state: "CA" }]
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+  });
+});
