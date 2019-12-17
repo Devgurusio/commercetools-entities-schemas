5a83148f8dafef89ae4e237917710a7f0760d4ab
Federico Moro del Álamo <fede@devgurus.io>
2019-12-17 15:25:34 +0100

    Add myPayment tests

---
+++ myPayment/__tests__/myTransactionDraft.spec.js
@@ -0,0 +1,128 @@
+const Ajv = require("ajv");
+const schema = require("../myTransactionDraft");
+
+describe("myTransactionDraft", () => {
+  let ajv;
+  let data;
+  let valid;
+
+  describe("when success", () => {
+    describe("when only required fields", () => {
+      beforeAll(() => {
+        data = {
+          type: "Authorization",
+          amount: {
+            type: "centPrecision",
+            currencyCode: "EUR",
+            centAmount: 390,
+            fractionDigits: 2
+          }
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        expect(valid).toBeTruthy();
+      });
+    });
+
+    describe("when all fields", () => {
+      beforeAll(() => {
+        data = {
+          timestamp: "2017-10-04T14:31:41.643Z",
+          type: "Authorization",
+          amount: {
+            type: "centPrecision",
+            currencyCode: "EUR",
+            centAmount: 390,
+            fractionDigits: 2
+          },
+          interactionId: "interactionId"
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        expect(valid).toBeTruthy();
+      });
+    });
+  });
+
+  describe("when error", () => {
+    describe("when missing type property", () => {
+      beforeAll(() => {
+        data = {
+          timestamp: "2017-10-04T14:31:41.643Z",
+          amount: {
+            type: "centPrecision",
+            currencyCode: "EUR",
+            centAmount: 390,
+            fractionDigits: 2
+          },
+          interactionId: "interactionId"
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+
+    describe("when type property is not an allowed one", () => {
+      beforeAll(() => {
+        data = {
+          timestamp: "2017-10-04T14:31:41.643Z",
+          type: "notAllowedType",
+          amount: {
+            type: "centPrecision",
+            currencyCode: "EUR",
+            centAmount: 390,
+            fractionDigits: 2
+          },
+          interactionId: "interactionId"
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+
+    describe("when missing amount property", () => {
+      beforeAll(() => {
+        data = {
+          timestamp: "2017-10-04T14:31:41.643Z",
+          type: "Authorization",
+          interactionId: "interactionId"
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+  });
+});
