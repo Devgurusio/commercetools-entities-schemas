d01c0d5b61d4e581fa36e4bf8aae0436d8ada07d
Federico Moro del Álamo <fede@devgurus.io>
2019-12-19 13:23:03 +0100

    Type tests

---
+++ type/__tests__/typeDraft.spec.js
@@ -0,0 +1,116 @@
+const Ajv = require("ajv");
+const schema = require("../typeDraft");
+
+describe("typeDraft", () => {
+  let ajv;
+  let data;
+  let valid;
+
+  describe("when success", () => {
+    describe("when only required fields", () => {
+      beforeAll(() => {
+        data = {
+          key: "key",
+          name: { en: "name", es: "nombre" }
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        expect(valid).toBeTruthy();
+      });
+    });
+
+    describe("when all fields", () => {
+      beforeAll(() => {
+        data = {
+          key: "key",
+          name: { en: "name", es: "nombre" },
+          description: { en: "description", es: "description" },
+          resourceTypeIds: ["asset"],
+          fieldDefinitions: [
+            {
+              type: { name: "String" },
+              name: "test-fieldDefinition",
+              label: { en: "Test Field Definition " },
+              required: false,
+              inputHint: "SingleLine"
+            }
+          ]
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should be valid", () => {
+        console.log(ajv.errors);
+        expect(valid).toBeTruthy();
+      });
+    });
+  });
+
+  describe("when error", () => {
+    describe("when missing key property", () => {
+      beforeAll(() => {
+        data = {
+          name: { en: "name", es: "nombre" },
+          description: { en: "description", es: "description" },
+          resourceTypeIds: ["asset"],
+          fieldDefinitions: [
+            {
+              type: { name: "String" },
+              name: "test-fieldDefinition",
+              label: { en: "Test Field Definition " },
+              required: false,
+              inputHint: "SingleLine"
+            }
+          ]
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+
+    describe("when missing name property", () => {
+      beforeAll(() => {
+        data = {
+          key: "key",
+          description: { en: "description", es: "description" },
+          resourceTypeIds: ["asset"],
+          fieldDefinitions: [
+            {
+              type: { name: "String" },
+              name: "test-fieldDefinition",
+              label: { en: "Test Field Definition " },
+              required: false,
+              inputHint: "SingleLine"
+            }
+          ]
+        };
+        ajv = Ajv();
+      });
+
+      beforeEach(() => {
+        valid = ajv.validate(schema, data);
+      });
+
+      test("should not be valid", () => {
+        expect(valid).toBeFalsy();
+      });
+    });
+  });
+});
