df30b8b66fa2c45c264ebd6866b35c9894b34740
Federico Moro <fmoro@users.noreply.github.com>
2022-03-03 14:03:16 +0100

    Fix order from cart draft and cart and order origin attribute (#33)
    Co-authored-by: Federico Moro <fmoro@dminc.com>

--- order/orderFromCartDraft.js
+++ order/orderFromCartDraft.js
@@ -12,19 +12,27 @@ module.exports = {
         'ResourceIdentifier to the Cart from which this order is created.'
     },
     version: {
-      type: 'number'
+      type: 'number',
+      description: 'The version of the cart from which an order is created.'
     },
     orderNumber: {
       type: 'string',
       description:
-        "String that uniquely identifies an order. It can be used to create more human-readable (in contrast to ID) identifier for the order. It should be unique across a project. Once it's set it cannot be changed. For easier use on Get, Update and Delete actions we suggest assigning order numbers that match the regular expression [a-z0-9_-]{2,36}."
+        "String that uniquely identifies an order. It can be used to create more human-readable (in contrast to ID) identifier for the order. It should be unique across a project. Once it's set it cannot be changed. For easier use on Get, Update and Delete actions we suggest assigning order numbers that match the regular expression [a-z0-9_-]{2,36}.",
+      pattern: '^[a-zA-Z0-9-_]+$',
+      minLength: 2,
+      maxLength: 36
     },
     paymentState,
-    orderState,
+    orderState: {
+      ...orderState,
+      description: 'Order will be created with Open status by default.'
+    },
     state: {
       ...reference,
       description: 'This reference can point to a state in a custom workflow.'
     },
     shipmentState
-  }
+  },
+  required: ['cart', 'version']
 };
