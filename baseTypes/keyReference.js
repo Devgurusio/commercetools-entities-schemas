d006acf8c450e7a1ad26f3860e3713ea6d184096
Federico Moro <fmoro@users.noreply.github.com>
2022-04-07 19:32:14 +0200

    Fix keyReference (#39)
    Co-authored-by: Federico Moro <fmoro@dminc.com>

--- baseTypes/keyReference.js
+++ baseTypes/keyReference.js
@@ -1,11 +1,9 @@
-const referenceType = require('./attributeType/referenceType');
-
 module.exports = {
   description: 'A representation of a Key Reference',
   properties: {
     typeId: {
-      ...referenceType,
-      description: 'The type of referenced resource'
+      description: 'The type of referenced resource',
+      type: 'string'
     },
     key: {
       description: 'The unique and immutable key of the referenced resource',
