9dca3766ae1081f53c52f9be9d7b99cc566a67fd
Federico Moro <fmoro@dminc.com>
2022-04-12 12:17:58 +0200

    Adapt httpApi/queryParams to sdk-client-v2 library

--- httpApi/queryParams.js
+++ httpApi/queryParams.js
@@ -2,6 +2,52 @@ module.exports = {
   type: 'object',
   description: 'Parameters accepted in CommerceTools queries',
   properties: {
+    expand: {
+      oneOf: [
+        {
+          type: 'array',
+          description: 'Attributes to expand',
+          items: {
+            type: 'string',
+            description: 'Attribute to expand'
+          }
+        },
+        {
+          type: 'string',
+          description: 'Attribute to expand'
+        }
+      ]
+    },
+    sort: {
+      oneOf: [
+        {
+          type: 'array',
+          description: 'Sort attribute',
+          items: {
+            type: 'string',
+            description: `Sort attribute.
+              I.E: createdAt asc`
+          }
+        },
+        {
+          type: 'string',
+          description: `Sort attribute.
+              I.E: createdAt asc`
+        }
+      ]
+    },
+    limit: {
+      type: 'integer',
+      description: 'Limit',
+      minimum: 1,
+      maximum: 500
+    },
+    offset: {
+      type: 'integer',
+      description: `The offset supplied by the client or the server default.
+        It is the number of elements skipped, not a page number`,
+      minimum: 0
+    },
     where: {
       oneOf: [
         {
@@ -22,85 +68,6 @@ module.exports = {
       type: 'string',
       description: 'Where operator to join where clauses',
       enum: ['and', 'or']
-    },
-    expand: {
-      oneOf: [
-        {
-          type: 'array',
-          description: 'Attributes to expand',
-          items: {
-            type: 'string',
-            description: 'Attribute to expand'
-          }
-        },
-        {
-          type: 'string',
-          description: 'Attribute to expand'
-        }
-      ]
-    },
-    page: {
-      type: 'integer',
-      description: 'Page number to retrieve',
-      minimum: 1,
-      default: 1
-    },
-    perPage: {
-      type: 'integer',
-      description: 'Number of items to retrieve on one page',
-      minimum: 1,
-      maximum: 500,
-      default: 20
-    },
-    sort: {
-      oneOf: [
-        {
-          type: 'array',
-          description: 'Sort attributes',
-          items: {
-            type: 'object',
-            properties: {
-              by: {
-                type: 'string',
-                description: 'Field to sort by'
-              },
-              direction: {
-                type: 'string',
-                description: 'Sort direction',
-                enum: ['asc', 'desc'],
-                default: 'asc'
-              }
-            },
-            required: ['by']
-          }
-        },
-        {
-          type: 'object',
-          properties: {
-            by: {
-              type: 'string',
-              description: 'Field to sort by'
-            },
-            direction: {
-              type: 'string',
-              description: 'Sort direction',
-              enum: ['asc', 'desc'],
-              default: 'asc'
-            }
-          },
-          required: ['by']
-        }
-      ]
-    },
-    sortBy: {
-      type: 'string',
-      description: 'Field name to apply the sort'
-    },
-    sortDirection: {
-      type: 'string',
-      description: 'Direction of order to apply',
-      enum: ['asc', 'desc'],
-      default: 'asc'
     }
   }
 };
